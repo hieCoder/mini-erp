d3d268a87fdc3e15083033b9c1e052ca12261081
huynhcanh <canh.shsoftvina@gmail.com>
2023-09-30 09:22:50 +0700

    add enum-package

---
+++ src/main/java/com/shsoftvina/erpshsoftvina/enums/user/PositionEnum.java
@@ -0,0 +1,20 @@
+package com.shsoftvina.erpshsoftvina.enums.user;
+
+public enum PositionEnum {
+
+    INTERN("Intern"), JUNIOR("Junior"), SENIOR("Senior"), MANAGER("Manager"), SENIOR_MANAGER("Senior Manager"), OWNER("Owner");
+
+    private String value;
+
+    PositionEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/shsoftvina/erpshsoftvina/enums/user/TypeUserEnum.java
@@ -0,0 +1,5 @@
+package com.shsoftvina.erpshsoftvina.enums.user;
+
+public enum TypeUserEnum {
+    PROBATION, OFFICIAL, PARTTIME
+}
