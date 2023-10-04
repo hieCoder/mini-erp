0729e0aee7a29746064af991ff377669f8e6644d
PC12\Admin <vtuyenlam1998@gmail.com>
2023-10-04 09:01:13 +0700

    demo testing...

---
+++ src/main/java/com/shsoftvina/erpshsoftvina/controller/AccountingController.java
@@ -0,0 +1,4 @@
+package com.shsoftvina.erpshsoftvina.controller;
+
+public class AccountingController {
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    void assertionWorked() {
+        int actual = 1 + 1;
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+}
