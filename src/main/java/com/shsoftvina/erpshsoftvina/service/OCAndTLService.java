a474f9b2e002a0f88f0df4aeb661045a4100eec3
huynhcanh <canh.shsoftvina@gmail.com>
2023-10-14 19:45:05 +0700

    .

--- src/main/java/com/shsoftvina/erpshsoftvina/service/OCAndTLService.java
+++
@@ -1,9 +0,0 @@
-package com.shsoftvina.erpshsoftvina.service;
-
-import com.shsoftvina.erpshsoftvina.model.response.oc_and_tl.OCAndTLOfOneDayResponse;
-
-import java.util.List;
-
-public interface OCAndTLService {
-    List<OCAndTLOfOneDayResponse> getAllByUserId(String userId);
-}
