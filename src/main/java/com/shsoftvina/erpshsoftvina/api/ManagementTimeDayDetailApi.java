481a5d94d6ce029b06bba6ef21218f6abfd467fd
huynhcanh <canh.shsoftvina@gmail.com>
2023-10-17 17:54:09 +0700

    canh2

---
+++ src/main/java/com/shsoftvina/erpshsoftvina/api/ManagementTimeDayDetailApi.java
@@ -0,0 +1,19 @@
+package com.shsoftvina.erpshsoftvina.api;
+
+import com.shsoftvina.erpshsoftvina.model.request.managementtime.day.DayCreateRequest;
+import com.shsoftvina.erpshsoftvina.model.request.managementtime.daydetail.DayDetailCreateRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/management-time-detail")
+public class ManagementTimeDayDetailApi {
+
+    @PostMapping("/day")
+    public ResponseEntity<?> createDay(@RequestBody DayDetailCreateRequest dayDetailCreateRequest) {
+        return ResponseEntity.ok(null);
+    }
+}
