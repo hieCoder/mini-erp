a8d81e16bf7fba6696c58e6850db82f5e868bfa8
huynhcanh <canh.shsoftvina@gmail.com>
2023-10-26 18:03:03 +0700

    canh

--- src/main/java/com/shsoftvina/erpshsoftvina/controller/TaskController.java
+++ src/main/java/com/shsoftvina/erpshsoftvina/controller/TaskController.java
@@ -1,5 +1,9 @@
 package com.shsoftvina.erpshsoftvina.controller;
 
+import com.shsoftvina.erpshsoftvina.entity.Task;
+import com.shsoftvina.erpshsoftvina.mapper.TaskMapper;
+import com.shsoftvina.erpshsoftvina.utils.ApplicationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @Autowired
+    private ApplicationUtils applicationUtils;
+
     @GetMapping
     public ModelAndView getTasks() {
         ModelAndView mav = new ModelAndView("task/tasks");
@@ -18,6 +28,10 @@ public class TaskController {
 
     @GetMapping("/{id}")
     public ModelAndView getTask(@PathVariable String id) {
+
+        Task task = taskMapper.findById(id);
+        applicationUtils.checkUserAllow(task.getUser().getId());
+
         ModelAndView mav = new ModelAndView("task/task-detail");
         mav.addObject("id", id);
         return mav;
