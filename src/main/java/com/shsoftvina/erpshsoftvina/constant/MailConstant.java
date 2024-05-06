77a3ca04a407c639698f5910e7e3b57994a9ccf7
PC11\Admin <quankhongmac@gmail.com>
2024-05-06 15:24:10 +0700

    edit content email

--- src/main/java/com/shsoftvina/erpshsoftvina/constant/MailConstant.java
+++ src/main/java/com/shsoftvina/erpshsoftvina/constant/MailConstant.java
@@ -5,6 +5,6 @@ public class MailConstant {
     public final static String REGISTER_SUBJECT = "CONFIRM NEW USER REGISTRATION";
     public final static String REGISTER_CONTENT =
             "<p>Your account have been registered!!!</p>" +
-            "<p>Please login through this <b>link</b>: http://localhost:8080/login</p>" +
+            "<p>Please login through this <b>link</b>: http://192.168.1.250:8080/login</p>" +
                     "<hr><img src='cid:logoImage' />";
 }
